6aa4472c24a6c5fd473df2b1300d62247976537f
Jianle <1091661451@qq.com>
2024-01-28 19:14:11 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/Smart_home.elf.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Smart_home.elf.dir/Core/Src/dht11.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Core/Src/gpio.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Core/Src/i2c.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Core/Src/main.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Core/Src/oled.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Core/Src/stm32f1xx_it.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Core/Src/syscalls.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Core/Src/system_stm32f1xx.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Core/Src/tim.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_i2c.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "CMakeFiles/Smart_home.elf.dir/startup/startup_stm32f103xb.s.obj"
+  "Smart_home.elf"
+  "Smart_home.elf.manifest"
+  "Smart_home.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/Smart_home.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ log.md
@@ -0,0 +1,35 @@
+##### 2024/1/21
+
+进度：OLED显示
+
+问题：
+
+CMake Error: Could not find cmake module file: CMakeDeterminehomeCompiler.cmake
+CMake Error: Error required internal CMake variable not set, cmake may not be built correctly.
+
+解决：删去工程文件名中的空格
+
+
+
+##### 2024/1/24
+
+进度：添加DHT11相关文件
+
+问题：
+
+arm-none-eabi-gcc.exe: error: Core/Inc/dht11.h: No such file or directory
+arm-none-eabi-gcc.exe: error: Core/Src/dht11.c: No such file or directory
+
+解决：通过参考：Clion 2020.2.4 CMake报错，发现新加入的文件后CmakeList.txt会自动添加LINKER_SCRIPT。将其后面的头文件和源文件删除，既恢复到set(LINKER_SCRIPT ${CMAKE_SOURCE_DIR}/STM32F103RFTx_FLASH.ld)
+
+
+
+##### 2024/1/25
+
+进度：完成了温湿度数值的正确显示
+
+问题：
+
+温湿度数值显示只有一位
+
+解决：sizeof(Data[0])函数获取的是元素的字节数，故都为1，只显示一位
